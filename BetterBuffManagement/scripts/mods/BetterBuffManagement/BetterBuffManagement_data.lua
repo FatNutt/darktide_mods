5a968e320e3c1ac9ea609e761d557d4a2aa1458b
FatNutt <fatnutt@pm.me>
2023-10-09 15:40:42 -0500

    feat(bbm) | added basic settings for mod

--- BetterBuffManagement/scripts/mods/BetterBuffManagement/BetterBuffManagement_data.lua
+++ BetterBuffManagement/scripts/mods/BetterBuffManagement/BetterBuffManagement_data.lua
@@ -4,4 +4,23 @@ return {
     name = mod:localize('mod_name'),
     description = mod:localize('mod_description'),
     is_togglable = true,
+    options = {
+        widgets = {
+            {
+                setting_id = 'toggle_hidden_buffs',
+                type = 'checkbox',
+                default_value = false
+            },
+            {
+                setting_id = 'add_buff_from',
+                type = 'dropdown',
+                default_value = 'end',
+                options = {
+                    { text = 'add_buff_from_option_end', value = 'end' },
+                    { text = 'add_buff_from_option_middle', value = 'middle' },
+                    { text = 'add_buff_from_option_start', value = 'start' }
+                }
+            }
+        }
+    }
 }
--- BetterBuffManagement/scripts/mods/BetterBuffManagement/BetterBuffManagement_localization.lua
+++ BetterBuffManagement/scripts/mods/BetterBuffManagement/BetterBuffManagement_localization.lua
@@ -5,4 +5,19 @@ return {
     mod_description = {
         en = 'Hide, group, and separate buffs into separate bars. Pairs best with custom hud for easy positioning',
     },
+    toggle_hidden_buffs = {
+        en = 'Toggle hidden buffs',
+    },
+    add_buff_from = {
+        en = 'Select where buffs are add from'
+    },
+    add_buff_from_option_end = {
+        en = 'End'
+    },
+    add_buff_from_option_middle = {
+        en = 'Middle'
+    },
+    add_buff_from_option_start = {
+        en = 'Start'
+    }
 }
